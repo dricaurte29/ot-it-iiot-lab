1db2853c4d49c43e2f9eecc58cbad478ae8bd4f2
David Ricaurte <david.andres.lima1@h-partners.com>
2026-07-03 15:03:39 -0500

    update devspace

---
+++ level1-plc/server.py
@@ -0,0 +1,8 @@
+import time
+
+print("🤖 [PLC VIRTUAL] Inicializando Servidor OPC-UA...")
+print("📡 Esperando configuración de variables industriales...")
+
+while True:
+    print("🔄 PLC corriendo... (Listo para recibir lógica del laboratorio)")
+    time.sleep(5)
